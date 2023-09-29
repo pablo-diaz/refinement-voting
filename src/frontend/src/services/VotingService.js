271939e09dc9cc2ab935fc7425db68effca3d008
Pablo Diaz <pablo.diaz@yuxiglobal.com>
2023-09-29 16:50:08 -0500

    Adding docker capabilities

--- src/frontend/src/services/VotingService.js
+++ src/frontend/src/services/VotingService.js
@@ -1,46 +1,52 @@
 import axios from "axios";
 
-const apiSetup = {
-    backendBaseUrl: 'https://localhost:7177/api/votingRoom'
-}
+const getBackendBaseUrl = async () =>
+    (JSON.parse(
+        JSON.stringify(
+            (await axios.get("ui-config.json")).data
+        )
+    )).backendBaseUrl;
 
 const createVotingRoom = async withLeaderName => {
     const postBody = { LeaderName: withLeaderName };
-    const { data } = await axios.post(`${apiSetup.backendBaseUrl}/newRoom`, postBody);
+    const { data } = await axios.post(`${await getBackendBaseUrl()}/newRoom`, postBody);
     return data;
 };
 
 const joinVotingRoom = async (withMemberName, withRoomId) => {
     const postBody = { MemberName: withMemberName, ToRoomId: withRoomId };
-    const { data } = await axios.post(`${apiSetup.backendBaseUrl}/newMember`, postBody);
+    const { data } = await axios.post(`${await getBackendBaseUrl()}/newMember`, postBody);
     return data;
 };
 
 const requestListMembers = async (inRoomId, forMemberId) => {
     const postBody = { };
-    await axios.post(`${apiSetup.backendBaseUrl}/room/${inRoomId}/member/${forMemberId}/getListOfMembers`, postBody);
+    await axios.post(`${await getBackendBaseUrl()}/room/${inRoomId}/member/${forMemberId}/getListOfMembers`, postBody);
 };
 
 const createNewVotingSession = async (inRoomId) => {
     const postBody = { };
-    await axios.post(`${apiSetup.backendBaseUrl}/room/${inRoomId}/newVotingSession`, postBody);
+    await axios.post(`${await getBackendBaseUrl()}/room/${inRoomId}/newVotingSession`, postBody);
 };
 
 const revealVotingResults = async (inRoomId) => {
     const postBody = { };
-    await axios.post(`${apiSetup.backendBaseUrl}/room/${inRoomId}/revealResults`, postBody);
+    await axios.post(`${await getBackendBaseUrl()}/room/${inRoomId}/revealResults`, postBody);
 };
 
 const submitVote = async (inRoomId, forMemberId, vote) => {
     const postBody = { };
-    await axios.post(`${apiSetup.backendBaseUrl}/room/${inRoomId}/member/${forMemberId}/vote/${vote}`, postBody);
+    await axios.post(`${await getBackendBaseUrl()}/room/${inRoomId}/member/${forMemberId}/vote/${vote}`, postBody);
 };
 
 const hookToServerSentEventsStream = (forMemberId, onMessageFn) => {
-    const evtSource = new EventSource(`${apiSetup.backendBaseUrl}/member/${forMemberId}/stream`);
-    evtSource.onmessage = theEvent => {
-        onMessageFn(JSON.parse(theEvent.data));
-    };
+    getBackendBaseUrl()
+        .then(baseUrl => {
+            const evtSource = new EventSource(`${baseUrl}/member/${forMemberId}/stream`);
+            evtSource.onmessage = theEvent => {
+                onMessageFn(JSON.parse(theEvent.data));
+            };
+        });
 }
 
 export default {
